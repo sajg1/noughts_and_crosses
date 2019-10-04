6be6832f4534a812213754df4bd196ca2e7155dd
Steve Gossip <stephengossip@gmail.com>
2019-10-04 16:20:11 +0100

    added the turnClick function that calls the turn funciton. The turn function fills the correct space on the board with the correct players character.

--- script.js
+++ script.js
@@ -26,5 +26,15 @@ function startGame() {
   for( var i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
     cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
   }
+}
+
+function turnClick(square) {
+  turn(square.target.id, humanPlayer)
+}
+
+function turn(squareId, player) {
+  origBoard[squareId] =  player;
+  document.getElementById(squareId).innerText = player;
 }
