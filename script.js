4f27fdab1436203092e5cb9bb3dbb58355ecabcf
Steve Gossip <stephengossip@gmail.com>
2019-10-04 16:07:27 +0100

    added the first code to script.js which includes the const for all possible winning combos, the characters to be used by human and ai and the start game function which is well annotated.

---
+++ script.js
@@ -0,0 +1,30 @@
+var origBoard;
+const humanPlayer = "0";
+const aiPlayer = "X";
+const winCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+// This selects all instances that are of the class "cell"
+const cells = document.querySelectorAll(".cell");
+startGame();
+
+
+function startGame() {
+  // This is so that when you hit replay after the eng of a game, it will reset the display to none. If not, the engame box will still be present
+  document.querySelector(".endgame").style.display = "none";
+  // A quick way to create an array of keys. Array.from creates a shallow array from an iterable object. In this case we create an array from an array of 9 keys.
+  origBoard = Array.from(Array(9).keys());
+  // This for loop will clear all text in each cell and remove the background color from any of the winning cells.
+  for( var i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+  }
+}
